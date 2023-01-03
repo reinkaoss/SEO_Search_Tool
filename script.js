fbdaf76e340dd489e340c9613101a95ee0b6c268
victorbiscio <victorbiscio1@hotmail.com>
2023-01-03 18:08:05 +0000

    adding items to the array of objects

--- script.js
+++ script.js
@@ -1,47 +1,547 @@
-comps = [ {
-  id: "EY",
-  London: "6 More London Pl, London SE1 2AF", 
-  Birmingham: "1 Colmore Circus Queensway, Birmingham B4 6AJ" }, 
+var comps = [
   {
-  id: "Aldi",
-  Birmingham: "Holly Ln Atherstone Birmingham CV9 2SQ",
-  London: "Holly Ln, Atherstone CV9 2SQ, United Kingdom", 
-},
-{
-  id: "BMW",
-  London: "Park Lane, BMW Park Lane Ltd, 70 Park Ln, London W1K 7TT",
-  Solihull: "Blythe Valley Park, Rybrook Specialist Technical Centre, Platinum, Central Blvd, Solihull B90 8AS", 
-},
-];
+    name: "Accenture",
+    address: {
+      street: "1 London Wall Place",
+      city: "London",
+      country: "UK",
+      postCode: "EC2Y 5AU"
+    }
+  },
+  {
+    name: "Adidas",
+    address: {
+      street: "Adidas House, Phoenix Way",
+      city: "Stockley Park",
+      country: "UK",
+      postCode: "UB11 1AZ"
+    }
+  },
+  {
+    name: "Agilent Technologies Ltd",
+    address: {
+      street: "Harlow Business Park, Edinburgh Way",
+      city: "Harlow",
+      country: "UK",
+      postCode: "CM20 2ZR"
+    }
+  },
+  {
+    name: "Air France-KLM Group",
+    address: {
+      street: "Air France House, 45-59 Compton Street",
+      city: "London",
+      country: "UK",
+      postCode: "EC1V 0DY"
+    }
+  },
+  {
+    name: "Alliance & Leicester",
+    address: {
+      street: "St. Peter's Square",
+      city: "Manchester",
+      country: "UK",
+      postCode: "M2 3LQ"
+    }
+  },
+  {
+    name: "Alstom",
+    address: {
+      street: "9th Floor, The White Chapel Building, 10 Whitechapel High St",
+      city: "London",
+      country: "UK",
+      postCode: "E1 8QS"
+    }
+  },
+  {
+    name: "Amadeus IT Group",
+    address: {
+      street: "Victoria House, Southampton Row",
+      city: "London",
+      country: "UK",
+      postCode: "WC1B 4DA"
+    }
+  },
+  {
+    name: "American Express",
+    address: {
+      street: "3 Sheldon Square, Paddington Central",
+      city: "London",
+      country: "UK",
+      postCode: "W2 6TT"
+    }
+  },
+  {
+    name: "Arcadia group",
+    address: {
+      street: "Colegrave House, 70 Berners Street",
+      city: "London",
+      country: "UK",
+      postCode: "W1T 3NL"
+    }
+  },
+  {
+    name: "ASDA",
+    address: {
+      street: "ASDA House, Southbank, Great Wilson Street",
+      city: "Leeds",
+      country: "UK",
+      postCode: "LS11 5AD"
+    }
+  },
+  {
+    name: "AstraZeneca",
+    address: {
+      street: "Silk Road, Macclesfield",
+      city: "Cheshire",
+      country: "UK",
+      postCode: "SK10 2NA"
+    }
+  },
+  {
+        name: "Audit Commission",
+        address: {
+          street: "Treasury Building, 1 Horse Guards Road",
+          city: "London",
+          country: "UK",
+          postCode: "SW1A 2HQ"
+        }
+      },
+      {
+        name: "AXA",
+        address: {
+          street: "5 Old Broad Street",
+          city: "London",
+          country: "UK",
+          postCode: "EC2N 1AD"
+        }
+      },
+      {
+        name: "BAE Systems",
+        address: {
+          street: "Warwick House, 1 Great George Street",
+          city: "London",
+          country: "UK",
+          postCode: "SW1P 3AE"
+        }
+      },
+      {
+        name: "Bank of England",
+        address: {
+          street: "Threadneedle Street",
+          city: "London",
+          country: "UK",
+          postCode: "EC2R 8AH"
+        }
+      },
+      {
+        name: "Barclays",
+        address: {
+          street: "1 Churchill Place",
+          city: "London",
+          country: "UK",
+          postCode: "E14 5HP"
+        }
+      },
+      {
+        name: "BASF",
+        address: {
+          street: "36 St James's Square",
+          city: "London",
+          country: "UK",
+          postCode: "SW1Y 4JH"
+        }
+      },
+      {
+        name: "BDO",
+        address: {
+          street: "5 Aldermanbury Square",
+          city: "London",
+          country: "UK",
+          postCode: "EC2V 7HR"
+        }
+      },
+      {
+        name: "Birmingham Airport",
+        address: {
+          street: "Birmingham Airport, B26 3QJ",
+          city: "Birmingham",
+          country: "UK",
+          postCode: "B26 3QJ"
+        }
+      },
+      {
+        name: "BMW Group",
+        address: {
+          street: "8-10 Great Eastern Street",
+          city: "London",
+          country: "UK",
+          postCode: "EC2A 3ER"
+        }
+      },
+      {
+        name: "BNP Paribas",
+        address: {
+          street: "78 Cannon Street",
+          city: "London",
+          country: "UK",
+          postCode: "EC4N 6AG"
+        }
+      },
+      {
+        name: "Boots",
+        address: {
+          street: "1 Thane Road West",
+          city: "Nottingham",
+          country: "UK",
+          postCode: "NG90 1BH"
+        }
+      },
+      {
+        name: "bp",
+        address: {
+          street: "1 St James's Square",
+          city: "London",
+          country: "UK",
+          postCode: "SW1Y 4JH"
+        }
+      },
+      {
+            name: "BAT",
+            address: {
+              street: "Globe House, 4 Temple Place",
+              city: "London",
+              country: "UK",
+              postCode: "WC2R 2PG"
+            }
+          },
+          {
+            name: "British Energy PLC",
+            address: {
+              street: "Maidenhead Office Park, Bray Road",
+              city: "Maidenhead",
+              country: "UK",
+              postCode: "SL6 1UB"
+            }
+          },
+          {
+            name: "Sky",
+            address: {
+              street: "Grant Way, Isleworth",
+              city: "Middlesex",
+              country: "UK",
+              postCode: "TW7 5QD"
+            }
+          },
+          {
+            name: "BT Group",
+            address: {
+              street: "BT Centre, 81 Newgate Street",
+              city: "London",
+              country: "UK",
+              postCode: "EC1A 7AJ"
+            }
+          },
+          {
+            name: "Cadbury",
+            address: {
+              street: "Chocolate Boulevard, Bournville",
+              city: "Birmingham",
+              country: "UK",
+              postCode: "B30 2LU"
+            }
+          },
+          {
+            name: "Capital Economics Ltd",
+            address: {
+              street: "13 Hanover Square",
+              city: "London",
+              country: "UK",
+              postCode: "W1S 1JY"
+            }
+          },
+          {
+            name: "Carillion",
+            address: {
+              street: "TETRIS, 58 Victoria Street",
+              city: "London",
+              country: "UK",
+              postCode: "SW1E 6QW"
+            }
+          },
+          {
+            name: "Caterpillar",
+            address: {
+              street: "100 New Bridge Street",
+              city: "London",
+              country: "UK",
+              postCode: "EC4V 6JA"
+            }
+          },
+          {
+            name: "Celestica Ltd",
+            address: {
+              street: "30 Daws Lane, Mill Hill",
+              city: "London",
+              country: "UK",
+              postCode: "NW7 4SD"
+            }
+          },
+          {
+            name: "Centrica",
+            address: {
+              street: "Millstream, Maidenhead Road",
+              city: "Windsor",
+              country: "UK",
+              postCode: "SL4 5GD"
+            }
+          },
+          {
+            name: "Cisco",
+            address: {
+              street: "140 Holborn",
+              city: "London",
+              country: "UK",
+              postCode: "EC1N 2NQ"
+            }
+          },
+          {
+            name: "Compass",
+            address: {
+              street: "Newburn House, 2 South Lambeth Road",
+              city: "London",
+              country: "UK",
+              postCode: "SW8 1RL"
+            }
+          },
+          {
+              name: "Phillips 66",
+              address: {
+                street: "One Aldgate, 3 Aldgate High Street",
+                city: "London",
+                country: "UK",
+                postCode: "EC3N 1BZ"
+              }
+            },
+            {
+              name: "Continental",
+              address: {
+                street: "270 Bath Road",
+                city: "Slough",
+                country: "UK",
+                postCode: "SL1 4DX"
+              }
+            },
+            {
+              name: "Corus Group",
+              address: {
+                street: "PO Box 1, Redcar Lane",
+                city: "Redcar",
+                country: "UK",
+                postCode: "TS10 1RD"
+              }
+            },
+            {
+              name: "Daimler Chrysler",
+              address: {
+                street: "DaimlerChrysler House, 75 London Wall",
+                city: "London",
+                country: "UK",
+                postCode: "EC2M 5NT"
+              }
+            },
+            {
+              name: "Debenhams",
+              address: {
+                street: "10 Brock St",
+                city: "Regent's Place",
+                country: "UK",
+                postCode: "NW1 3FG"
+              }
+            },
+            {
+              name: "Dell",
+              address: {
+                street: "1 Dell Way, The Square",
+                city: "Bracknell",
+                country: "UK",
+                postCode: "RG12 1QN"
+              }
+            },
+            {
+              name: "Deloitte",
+              address: {
+                street: "New Street Square, New Fetter Lane",
+                city: "London",
+                country: "UK",
+                postCode: "EC4A 3BZ"
+              }
+            },
+            {
+              name: "Delphi Technologies",
+              address: {
+                street: "7th Floor, South Wing, Portland House, Bressenden Place",
+                city: "London",
+                country: "UK",
+                postCode: "SW1E 5BH"
+              }
+            },
+            {
+              name: "Department of Trade and Industry",
+              address: {
+                street: "1 Victoria Street",
+                city: "London",
+                country: "UK",
+                postCode: "SW1H 0ET"
+              }
+            },
+            {
+              name: "Deutsche Bank",
+              address: {
+                street: "Winchester House, 1 Great Winchester Street",
+                city: "London",
+                country: "UK",
+                postCode: "EC2N 2DB"
+              }
+            },
+            {
+              name: "E.ON UK",
+              address: {
+                street: "Westwood Way, Westwood Business Park",
+                city: "Coventry",
+                country: "UK",
+                postCode: "CV4 8LG"
+              }
+            },
+              {
+                name: "Electrolux",
+                address: {
+                  street: "1 Swan Lane",
+                  city: "London",
+                  country: "UK",
+                  postCode: "EC4R 3TN"
+                }
+              },
+              {
+                name: "Eli Lilly",
+                address: {
+                  street: "Priestley Road, Basingstoke",
+                  city: "Hampshire",
+                  country: "UK",
+                  postCode: "RG24 9NL"
+                }
+              },
+              {
+                name: "EY",
+                address: {
+                  street: "25 Churchill Place",
+                  city: "London",
+                  country: "UK",
+                  postCode: "E14 5EY"
+                }
+              },
+              {
+                name: "ExxonMobil",
+                address: {
+                  street: "Ermyn House, Ermyn Way",
+                  city: "Leatherhead",
+                  country: "UK",
+                  postCode: "KT22 8UX"
+                }
+              },
+              {
+                name: "Ford Motor Company Ltd",
+                address: {
+                  street: "Eagle Way, Brentwood",
+                  city: "Essex",
+                  country: "UK",
+                  postCode: "CM13 3BW"
+                }
+              },
+              {
+                name: "Fujitsu",
+                address: {
+                  street: "22 Baker Street",
+                  city: "London",
+                  country: "UK",
+                  postCode: "W1U 3BW"
+                }
+              },
+              {
+                name: "George",
+                address: {
+                  street: "Imperial Place, Maxwell Road",
+                  city: "Borehamwood",
+                  country: "UK",
+                  postCode: "WD6 1JN"
+                }
+              },
+              {
+                name: "GSK",
+                address: {
+                  street: "980 Great West Road",
+                  city: "Brentford",
+                  country: "UK",
+                  postCode: "TW8 9GS"
+                }
+              }
+                        ];
+
+
+
+                      
+                        //     console.log(companyName)
+
 
 function findComp(){
-  x = document.getElementById("input").value
-var find = comps.find(item => item.id == x);
-for(var i=0; i < comps.length; i++)
-  if(find != undefined)
-  document.getElementById("x").innerHTML = JSON.stringify(find,null,"\n", 2);
-  else 
-  document.getElementById("x").innerHTML = "Company not found";
-console.log(find)
-return;
-}
 
-// Copy content to clipboard
-function myFunction() {
-  // Get the text field
-  var copyText = document.getElementById("x");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
+  inputEl = document.getElementById("input").value
+  var companyName = inputEl;
+  var company = comps.filter(function(c) {
+    return c.name === companyName;
+  })[0];
+  var companyTitle = company.name;
+  var address = company.address;
+  var street = address.street;
+  var city = address.city;
+  var country = address.country;
+  var postCode = address.postCode;
+  document.getElementById("x").innerHTML = companyTitle + ", " + street + ", " + city + ", " + country + ", " + postCode;
 }
 
 
-console.log(Object.entries(comps[1]))
-console.log(JSON.stringify(comps[1]))
+//   inputEl = document.getElementById("input").value
+//   var find = JSON.stringify(comps.name);
+// // for(var i=0; i < comps.length; i++)
+// //   if(find != undefined)
+// //   document.getElementById("x").innerHTML = JSON.stringify(find[i]);
+// for(var i=0; i < comps.length; i++)
+//   if(inputEl === comps[i]) {
+//     document.getElementById("x").innerHTML = 'found';
+    
+//   }
+//   else 
+//   document.getElementById("x").innerHTML = "Company not found";
+//   console.log(comps.name)
+// return;
+// }
+
+// // Copy content to clipboard
+// function myFunction() {
+//   // Get the text field
+//   var copyText = document.getElementById("x");
+
+//   // Select the text field
+//   copyText.select();
+//   copyText.setSelectionRange(0, 99999); // For mobile devices
+
+//    // Copy the text inside the text field
+//   navigator.clipboard.writeText(copyText.value);
+
+//   // Alert the copied text
+//   alert("Copied the text: " + copyText.value);
+// }
+
+
+// // console.log(Object.entries(comps[1]))
+// // console.log(JSON.stringify(comps[1]))
